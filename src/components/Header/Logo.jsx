4bf461172f52043de1d2b6438f8d477824d8afa5
Artem <artem.sopizhenko@quick.no>
2025-01-21 06:41:56 +0100

    - fix for logo

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,10 +1,9 @@
-import logo from "../../assets/logo.svg";
 import {Link} from "react-router-dom";
 
 const Logo = () => {
     return (
         <Link to="/" className="logo">
-            <img src={logo} alt="Logo"/>
+            <img src="/Logo.svg" alt="TravelTrucks"/>
         </Link>
     );
 };
